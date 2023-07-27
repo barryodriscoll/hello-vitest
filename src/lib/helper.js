9d380b3d818d711d12e21235a1cb34d7fc2c3eb4
Barry ODriscoll <barry.odriscoll@wazp.io>
2023-07-27 16:54:24 +0100

    partial commit with failing tests

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,4 +1,4 @@
-async function getCatFact() {
+async function getCatFactReal() {
 
     const response = await fetch('https://catfact.ninja/fact?max_length=256');
 
@@ -8,4 +8,18 @@ async function getCatFact() {
 
 }
 
-export { getCatFact }
+async function getCatFactToBeMocked() {
+    throw new Error('This method is to be mocked and should never executed'); 
+}
+
+async function getCatFactWithMockedFetch() {
+
+    const response = await fetch('https://catfact.ninja/fact?max_length=256');
+
+    const catFact = await response.json();
+
+    return catFact.fact
+
+}
+
+export { getCatFactReal, getCatFactToBeMocked, getCatFactWithMockedFetch }
--- src/lib/helper.test.js
+++ src/lib/helper.test.js
@@ -1,14 +1,26 @@
 import { describe, it, expect, afterEach, vi } from 'vitest';
-import { getCatFact } from './helper.js'
+import { getCatFactReal, getCatFactToBeMocked, getCatFactWithMockedFetch }from './helper.js'
 
-describe('test with mocking', async () => {
+describe('mocking examples', async () => {
+    afterEach(() => {
+        vi.restoreAllMocks()
+    })
+
+    it('get a cat fact from real api, might fail if api is down', async () => {
+        const result = await getCatFactReal()
+
+        expect(result).toBeTruthy()
+        expect(typeof result).toBe("string")
+    })
+
     it('get a cat fact from our mock instead of real api', async () => {
         const { mockedGetCatFact } = vi.hoisted(() => {
             return { mockedGetCatFact: vi.fn() }
         })
 
         vi.mock('./helper.js', () => {
-            return { getCatFact: mockedGetCatFact }
+            return { getCatFactToBeMocked: mockedGetCatFact,
+                     getCatFactReal: getCatFactReal }
         })
 
         mockedGetCatFact.mockReturnValue("Cats wag their tail when they're happy")
@@ -18,4 +30,6 @@ describe('test with mocking', async () => {
         expect(result).toBe("Cats wag their tail when they're happy")
         expect(mockedGetCatFact).toHaveBeenCalled()
     })
+
+    it
 })
